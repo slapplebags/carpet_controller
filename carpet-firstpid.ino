f2352379ef769942dfbda0ad344533070854e9bb
slapplebags <richardnixonkingofthemoleppl@gmail.com>
2024-04-03 22:50:01 -0700

    Update carpet-firstpid.ino

--- carpet-firstpid.ino
+++ carpet-firstpid.ino
@@ -18,9 +18,9 @@ DeviceSettings settings; // Create an instance of DeviceSettings
 bool lastLbuttonState = HIGH; // Assuming active low buttons
 bool lastRbuttonState = HIGH;
 
-const int LbuttonPin = 1; // The pin the button is connected to
-const int RbuttonPin = 2; // The pin the button is connected to
-const int CbuttonPin = 3; // The pin the button is connected to
+const int LbuttonPin = 2; // The pin the button is connected to
+const int RbuttonPin = 11; // The pin the button is connected to
+const int CbuttonPin = 10; // The pin the button is connected to
 volatile int loopState = 0;
 volatile int buttonPressed = 0; // Flag to indicate button press
 const long debounceDelay = 150; // Debounce delay in milliseconds
@@ -28,15 +28,17 @@ unsigned long lastDebounceTime = 0; // Last debounce check time
 unsigned long motorStartTime = 0; 
 enum MotorState {STOPPED, CLOCKWISE, COUNTERCLOCKWISE} motorState = STOPPED;
 enum MotorState lastMotorState = STOPPED; // Add this to track the last non-stopped state
-const int heaterPinCW = 21; // Heater control pin
-const int heaterPinCCW = 17; // Heater control pin
-const int fanPin = 18; // Heater control pin
-const int coilPin = 44; // Heater control pinsdfdf
-
+const int heaterPinCW = 43; // Heater control pin
+const int heaterPinCCW = 44; // Heater control pin
+const int fanPin = 17; // Heater control pin
+const int coilPin = 21; // Heater control pin
 const double thresholdCW = 0.8; // Threshold for CW close to 1
 const double thresholdCCW = 0.2; // Threshold for CCW close to 0
 
+double previousOutput = 0; // Initialize with 0 or an appropriate value.
+double Setpoint = 80;
 double heSetpoint, Input, Output, Input2, coilSetpoint, fanSetpoint, heMotorTime;
+
 // PID tuning parameters - These need to be adjusted based on your setup and requirements
 double Kp = 2.0, Ki = 5.0, Kd = 1.0;
 PID myPID(&Input, &Output, &settings.heSetpoint, Kp, Ki, Kd, DIRECT);
@@ -48,13 +50,13 @@ DallasTemperature sensors(&oneWire);
 
 void IRAM_ATTR handleButtonPress() {
   unsigned long currentTime = millis();
-
     buttonPressed = 1; // Set flag to indicate the button was pressed
-
   }
 
 
 // Addresses of 2 DS18B20s
+//uint8_t sensor1[8] = { 0x28, 0xCA, 0x64, 0x43, 0xD4, 0xE1, 0x3C, 0x03 };
+//uint8_t sensor2[8] = { 0x28, 0x75, 0x32, 0x43, 0xD4, 0xE1, 0x3C, 0x29 };
 uint8_t sensor1[8] = { 0x28, 0xB9, 0x98, 0x43, 0xD4, 0xE1, 0x3C, 0x45 };
 uint8_t sensor2[8] = { 0x28, 0xF5, 0x75, 0x43, 0xD4, 0xE1, 0x3C, 0x7B };
 
@@ -64,16 +66,20 @@ void setup() {
   pinMode(PIN_POWER_ON, OUTPUT);
   pinMode(fanPin, OUTPUT);
   pinMode(coilPin, OUTPUT);
+  pinMode(heaterPinCW, OUTPUT);
+pinMode(heaterPinCCW, OUTPUT);
   digitalWrite(PIN_POWER_ON, HIGH);
   digitalWrite(fanPin, LOW);
   digitalWrite(coilPin, LOW);
+  digitalWrite(heaterPinCW,LOW);
+  digitalWrite(heaterPinCCW, LOW);
   tft.begin();
   tft.fillScreen(TFT_BLACK);
   tft.setRotation(3);
   EEPROM.begin(sizeof(DeviceSettings));
   
   pinMode(CbuttonPin, INPUT_PULLUP); // Initialize the button pin as input with pull-up resistor
-  attachInterrupt(digitalPinToInterrupt(CbuttonPin), handleButtonPress, RISING); // Attach interrupt
+  attachInterrupt(digitalPinToInterrupt(CbuttonPin), handleButtonPress, RISING); // Attach interrupt  
 
     EEPROM.get(0, settings);
 
@@ -88,12 +94,14 @@ void setup() {
     EEPROM.put(0, settings);
     EEPROM.commit(); // Ensure data is written to flash
   }
+
   myPID.SetMode(AUTOMATIC); // Turn PID on
   myPID.SetOutputLimits(0, 1); // Set PID output limits to match digital on/off state
 
 }
 
 void loop() {
+
   if (buttonPressed) {
     noInterrupts(); // Disable interrupts to safely check shared variables
     buttonPressed = 0; // Reset the flag
@@ -101,18 +109,20 @@ void loop() {
     interrupts(); // Re-enable interrupts
     Serial.print("Loop State Changed to: ");
     Serial.println(loopState);
+        tft.fillScreen(TFT_BLACK);
   }
   sensors.requestTemperatures();
   Input = sensors.getTempF(sensor1); // Update current temperature from sensor
   Input2 = sensors.getTempF(sensor2);
+
   myPID.Compute();
 
   if (Input2 >= settings.fanSetpoint) {
 //    Serial.println("fan on");
-    digitalWrite(fanPin, HIGH);
+    digitalWrite(1, HIGH);
   } else {
 //    Serial.println("fan off");
-    digitalWrite(fanPin, LOW);
+    digitalWrite(1, LOW);
   }
 
   if (Input2 >= settings.coilSetpoint) {
@@ -122,6 +132,7 @@ void loop() {
 //    Serial.println("engine on");
     digitalWrite(coilPin, HIGH);
   }
+
 
     // Logic for changing motor direction based on PID output and previous state
     if (Output >= thresholdCW && motorState == STOPPED && lastMotorState != CLOCKWISE) {
@@ -144,10 +155,6 @@ void loop() {
         stopMotor();
     }
 
-
-  // Update previousOutput with the current Output value for the next iteration
-  //previousOutput = Output;
-
   switch (loopState) {
     case 0:
       loopOriginal();
@@ -169,9 +176,9 @@ void loop() {
       loopAlternate4();
               EEPROM.put(0, settings); // Optionally, save the new setting to EEPROM
               EEPROM.commit(); // Ensure data is written to flash
-              tft.fillScreen(TFT_BLACK);
       break;           
   }
+
 }
 
 void loopOriginal() {
@@ -190,7 +197,7 @@ void loopOriginal() {
   int temperatureFInt2 = (int)Input2; // Convert float to int for display
   itoa(temperatureFInt2, temperatureFString2, 10); // Convert int to string
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.drawString("EngTmp:", 0, 80, 4);
+  tft.drawString("EngTmp:", 0, 85, 4);
     if (settings.coilSetpoint > sensors.getTempF(sensor2)) { 
     tft.setTextColor(TFT_BLUE, TFT_BLACK);
   } else { 
@@ -222,8 +229,8 @@ void loopAlternate1() {
     // Display code
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.drawString("HeX Set Temp", 0, 80, 4);
-    tft.drawString(String(settings.heSetpoint, 1), 0, 100, 4); // Show current heSetpoint settings);
+    tft.drawString("HeX Set Temp", 0, 0, 4);
+    tft.drawString(String(settings.heSetpoint, 1), 0, 40, 8); // Show current heSetpoint settings);
 }
 
 void loopAlternate2() {
@@ -248,8 +255,8 @@ void loopAlternate2() {
     // Display code
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.drawString("Motor Runtime", 0, 80, 4);
-    tft.drawString(String(settings.heMotorTime, 1), 0, 100, 4); // Show current heSetpoint settings);
+    tft.drawString("Motor Runtime", 0, 0, 4);
+    tft.drawString(String(settings.heMotorTime, 0), 0, 40, 8); // Show current heSetpoint settings);
 }
 
 void loopAlternate3() {
@@ -274,8 +281,8 @@ void loopAlternate3() {
     // Display code
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.drawString("Fan Set Temp", 0, 80, 4);
-    tft.drawString(String(settings.fanSetpoint, 1), 0, 100, 4); // Show current heSetpoint settings);
+    tft.drawString("Fan Set Temp", 0, 0, 4);
+    tft.drawString(String(settings.fanSetpoint, 1), 0, 40, 8); // Show current heSetpoint settings);
 }
 
 void loopAlternate4() {
@@ -298,10 +305,9 @@ void loopAlternate4() {
     lastRbuttonState = currentRbuttonState;
 
     // Display code
-    tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.drawString("E-Stop Set Temp", 0, 80, 4);
-    tft.drawString(String(settings.coilSetpoint, 1), 0, 100, 4); // Show current heSetpoint settings);
+    tft.drawString("E-Stop Set Temp", 0, 0, 4);
+    tft.drawString(String(settings.coilSetpoint, 1), 0, 40, 8); // Show current heSetpoint settings);
 }
 
 void startMotorCW() {
